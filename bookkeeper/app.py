51f083ee981413439748320456bf246fd2ee2bcf
Vladislav Aleinik <valeinik@ispras.ru>
2023-03-07 23:57:28 +0300

    view: add expenses table

--- bookkeeper/app.py
+++ bookkeeper/app.py
@@ -2,7 +2,7 @@ import sys
 from PySide6 import QtWidgets
 from PySide6.QtCore import Qt
 
-from view.labeled import LabeledComboBoxInput
+from view.expense_table import LabeledExpenseTable
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
@@ -12,17 +12,24 @@ if __name__ == "__main__":
     window.setWindowTitle('Тестовый стенд')
     window.resize(350, 150)
 
+    # Data to be rendered:
+    data = [["2023-02-26 14:30:00", str( 100), "кофе",   ""],
+            ["2023-02-26 14:00:00", str(1000), "одежда", ""],
+            ["2023-02-25 18:30:00", str( 500), "мясо",   ""],
+            ["2023-02-20 19:46:00", str(  33), "вода",   ""],]
+
+    # Create expenses table:
+    expenses = LabeledExpenseTable()
+    expenses.add_data(data)
+
     # Create layout for window:
     horizontal_layout = QtWidgets.QHBoxLayout()
 
-    # Create checkboxes:
-    line_input = LabeledComboBoxInput(
-        "Какое животное вы кусаити?", ["собак", "капiбара", "андатра"])
+    horizontal_layout.addWidget(expenses)
 
-    horizontal_layout.addWidget(line_input)
+    window.setLayout(horizontal_layout)
 
     # Render window:
-    window.setLayout(horizontal_layout)
     window.show()
 
     sys.exit(app.exec())
---
+++ bookkeeper/view/expense_table.py
@@ -0,0 +1,61 @@
+from PySide6 import QtWidgets
+from PySide6.QtCore import Qt
+
+class ExpenseTableWidget(QtWidgets.QTableWidget):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        # Table configuration:
+        self.setColumnCount(4)
+        self.setRowCount(20)
+
+        headers = "Дата Сумма Категория Комментарий".split()
+        self.setHorizontalHeaderLabels(headers)
+
+        # Configure table header:
+        header = self.horizontalHeader()
+        header.setSectionResizeMode(
+            0, QtWidgets.QHeaderView.ResizeToContents)
+        header.setSectionResizeMode(
+            1, QtWidgets.QHeaderView.ResizeToContents)
+        header.setSectionResizeMode(
+            2, QtWidgets.QHeaderView.ResizeToContents)
+        header.setSectionResizeMode(
+            3, QtWidgets.QHeaderView.Stretch)
+
+        # Disable vertical header:
+        self.verticalHeader().hide()
+
+        # Enter edit mode on double-click:
+        self.setEditTriggers(
+            QtWidgets.QAbstractItemView.DoubleClicked)
+
+    def add_data(self, data: list[list[str]]):
+        for i, row in enumerate(data):
+            for j, item in enumerate(row):
+                self.setItem(
+                    i, j,
+                    QtWidgets.QTableWidgetItem(item.capitalize())
+                )
+
+class LabeledExpenseTable(QtWidgets.QGroupBox):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        # Label:
+        self.label = QtWidgets.QLabel("<b>Последние траты</b>")
+        self.label.setAlignment(Qt.AlignCenter)
+
+        # Expenses table:
+        self.table = ExpenseTableWidget()
+
+        # Vertical layout:
+        self.vbox = QtWidgets.QVBoxLayout()
+
+        self.vbox.addWidget(self.label)
+        self.vbox.addWidget(self.table)
+
+        self.setLayout(self.vbox)
+
+    def add_data(self, data: list[list[str]]):
+        self.table.add_data(data)
